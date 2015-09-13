4066bc9c09a63d7c2f99cf6d818af0e269f051cb
Nixorn <murchendaizer@gmail.com>
2015-09-13 19:41:16 +0300

    add map skipper

--- cragapp/admanager.py
+++ cragapp/admanager.py
@@ -218,7 +218,7 @@ class AdManager(scrapy.Spider):
             formdata ={"areaabb":self.area.clcode},
             method='POST',
             #headers={"Host":"accounts.craigslist.org",
-            #         #'Referer':'https://accounts.craigslist.org/login/home',
+            #         'Referer':'https://accounts.craigslist.org/login/home',
             #         'Connection':'keep-alive'},
             callback=self.add_serv)
 
@@ -332,13 +332,38 @@ class AdManager(scrapy.Spider):
                      'Connection':'keep-alive'},
 
             dont_filter=True,
+            callback=self.add_map)
+
+def add_map(self, response):#new map menu from CL
+    debug_html_content(response,"add_map",5)
+    if "do not show on maps" in response.body:
+        return scrapy.FormRequest.from_response(
+            response=response,
+            url = response.request.url.split("?s=")[0],
+            formdata ={
+                "xstreet0":"",
+                "xstreet1":"",
+                "city":"",
+                "region":"",
+                "postal":"",
+                "lat":"0",
+                "lng":"0",
+                "AreaID":"3",
+                "seenmap":"1",
+                "draggedpin":"0",
+                "clickedinclude":"0",
+                "geocoder_latitude":"",
+                "geocoder_longitude":"",
+                "geocoder_accuracy":"",
+                "geocoder_version":"",
+                "cryptedStepCheck":cryptedStepCheck},
+            method='POST',
             callback=self.add_images)
-
-
+    else: self.add_images(response)
 
 
     def add_images(self, response):#images
-        debug_html_content(response,"add_images",5)
+        debug_html_content(response,"add_images",6)
 
         images = Image.query.filter(Image.idads == self.ad.idads).all()
 
@@ -424,7 +449,7 @@ class AdManager(scrapy.Spider):
             callback=self.add_publish)
 
     def add_publish(self, response):#publish
-        debug_html_content(response,"add_publish",6)
+        debug_html_content(response,"add_publish",7)
         cryptedStepCheck = \
             response.xpath("//form[./input[@name='cryptedStepCheck']]"+\
                 "/input[@name='cryptedStepCheck']/@value").extract()[0]
@@ -442,7 +467,7 @@ class AdManager(scrapy.Spider):
             callback=self.add_get_id)
 
     def add_get_id(self,response):#get idcrag
-        debug_html_content(response,"add_get_id",7)
+        debug_html_content(response,"add_get_id",8)
         idcrag = response\
                  .xpath('//a[@target="_blank"]/@href')\
                  .extract_first().split('/')[-1].split('.')[0]
