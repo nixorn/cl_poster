026775b6a762d8dcad71c859a08ffff1ae1716cf
Nixorn <murchendaizer@gmail.com>
2015-10-07 19:44:35 +0300

    draft of adding ad

--- admanager.js
+++ admanager.js
@@ -5,12 +5,32 @@ var page   = require('webpage').create(),
     loadInProgress = false,
     crawlIndex = 0,
     CLlogin,
-    CLpassword;
+    CLpassword,
+    area_shortcode,
+    service_code,
+    title,            
+    body,             
+    specific_location,
+    postal,           
+    is_licensed,      
+    license_info;
+
+
+
 
 page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; rv:41.0) Gecko/20100101 Firefox/41.0"
 
-CLlogin    = 'murchendaizer@gmail.com'
-CLpassword = 'ltybcxtge'
+CLlogin    = 'murchendaizer@gmail.com';
+CLpassword = 'ltybcxtge';
+area_shortcode = 'isp'; //long island
+service_code = 83; //skilled trade
+title = 'Haskeller';            
+body = 'Good (or bad?) haskell programmer at your service';             
+specific_location = 'Babylon';
+postal = '11930';          
+is_licensed = 'no';
+license_info = 'Where I can take a haskell license, guys?';
+
 
 function loadLoginForm(){
     page.open('https://accounts.craigslist.org/login');};
@@ -20,13 +40,76 @@ function fillLoginData(){
 	function(CLlogin, CLpassword){
 	    document.getElementById('inputEmailHandle').value = CLlogin;
 	    document.getElementById('inputPassword').value = CLpassword;
-	    }, CLlogin, CLpassword);};
+	}, CLlogin, CLpassword);
+    page.render('1_login_form.png');
+}
 
 function submitLoginData(){
     page.evaluate(
 	function(){
-	    document.getElementsByName('login')[0].submit()});};
+	    document.getElementsByName('login')[0].submit()});}
+    
+function setAreaSelect(){
+    page.evaluate(
+	function(area_shortcode){
+	    document.getElementsByName('areaabb')[0].value = area_shortcode}, area_shortcode);
+    page.render('2_area_select.png');}
 
+function submitNewPosting(){
+    page.evaluate(
+	function(){
+	    document.getElementsByClassName('new_posting_thing')[0].submit()});
+
+}
+
+
+function selectServices1(){
+    page.evaluate(
+	function (){
+	    document.querySelectorAll("input[value=so]").checked = true});
+    page.render('3_services.png')}
+
+function selectServices2(){
+    page.evaluate(
+	function (service_code){
+	    document.querySelectorAll("input[value="+service_code+"]").checked = true}, service_code);
+    page.render('4_services.png')}
+
+
+function submitServices(){
+    page.evaluate(
+    function(){
+	document.querySelectorAll('form[class="catpick picker"]')[0].submit()})}
+
+function fillAdBody(){
+    page.evaluate(
+	function(title, body, specific_location, postal, is_licensed, license_info){
+	    
+	    document.getElementById('PostingTitle').value = title;
+	    document.getElementById('PostingBody').value = body;
+	    document.getElementById('GeographicArea').value = specific_location;
+	    document.getElementById('postal_code').value = postal;
+
+	    if (is_licensed == yes) {
+		document.getElementById('lic').checked = true;
+		document.getElementById('license_info').value = license_info;
+		
+	    } else{document.getElementById('nolic').checked = true;}
+	    
+	},title,body,specific_location,	postal,is_licensed,license_info);
+    page.render('5_ad_body.png');}
+
+function submitAdBody(){
+    page.evaluate(
+	function(){
+	    document.getElementById('postingForm').submit()})}
+
+
+function publish(){
+    page.render('6_pre_publish.png');
+    page.evaluate(
+	function(){
+	    document.getElementsByTagName('form')[0].submit()})}
 
 function finalize(){
     page.render('finalize.png')};
@@ -44,7 +127,18 @@ page.onLoadFinished = function() {
 };
 
 
-steps = [loadLoginForm, fillLoginData,submitLoginData,finalize]
+steps = [loadLoginForm,
+	 fillLoginData,
+	 submitLoginData,
+	 setAreaSelect,
+	 submitNewPosting,
+	 selectServices1,
+	 submitServices,
+	 selectServices2,
+	 submitServices,
+	 fillAdBody,
+	 
+	 finalize]
 
 interval = setInterval(function() {
   if (!loadInProgress && typeof steps[crawlIndex] == "function") {
