e62641f430fa16599f497e335ccaa339e049730f
Nixorn <murchendaizer@gmail.com>
2015-10-07 11:37:49 +0300

    login with ohantomjs

--- admanager.js
+++ admanager.js
@@ -2,37 +2,61 @@
 
 var page   = require('webpage').create(),
     system = require('system'),
-    address,
-    qry;
+    loadInProgress = false,
+    crawlIndex = 0,
+    CLlogin,
+    CLpassword;
 
-address = system.args[1];
-qry = system.args[2];
+page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; rv:41.0) Gecko/20100101 Firefox/41.0"
 
+CLlogin    = 'murchendaizer@gmail.com'
+CLpassword = 'ltybcxtge'
 
+function loadLoginForm(){
+    page.open('https://accounts.craigslist.org/login');};
 
-page.onLoadFinished = function(){
-    page.render('1.png');
-    //console.log(page.content);
-
-    page.evaluate( function(qry){document.getElementById('query').value = qry}, qry);
-
-    page.render('2.png');
-
+function fillLoginData(){
     page.evaluate(
-	function(){document.getElementById('search').submit();});
+	function(CLlogin, CLpassword){
+	    document.getElementById('inputEmailHandle').value = CLlogin;
+	    document.getElementById('inputPassword').value = CLpassword;
+	    }, CLlogin, CLpassword);};
 
-    page.render('3.png');
+function submitLoginData(){
+    page.evaluate(
+	function(){
+	    document.getElementsByName('login')[0].submit()});};
+
+
+function finalize(){
+    page.render('finalize.png')};
+
+
+
+page.onLoadStarted = function() {
+  loadInProgress = true;
+  console.log("load started");
+};
+
+page.onLoadFinished = function() {
+  loadInProgress = false;
+  console.log("load finished");
+};
+
+
+steps = [loadLoginForm, fillLoginData,submitLoginData,finalize]
+
+interval = setInterval(function() {
+  if (!loadInProgress && typeof steps[crawlIndex] == "function") {
+    console.log("step " + (crawlIndex + 1));
+    steps[crawlIndex]();
+    crawlIndex++;
+  }
+  if (typeof steps[crawlIndex] != "function") {
+    console.log("test complete!");
     phantom.exit();
-}
-
-if (system.args.length < 3){
-    console.log('you should give link and the query')
-    phantom.exit()
-} else {
-    console.log(address);
-    console.log(qry);
-    page.open(address);
-}
+  }
+}, 1500);
 
 
 
